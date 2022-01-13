acafb7b7391d08648578b983cd508f96b3fde96e
mgodiya <45644307+mgodiya@users.noreply.github.com>
2022-01-13 08:19:43 -0700

    Update vultr.py
    updating this file for APIv2 implementations

--- vultr/vultr.py
+++ vultr/vultr.py
@@ -1,41 +1,50 @@
 '''Python library for the Vultr cloud API'''
 from .utils import VultrBase
-from .v1_account import VultrAccount
-from .v1_app import VultrApp
-from .v1_backup import VultrBackup
-from .v1_dns import VultrDNS
-from .v1_firewall import VultrFirewall
-from .v1_iso import VultrISO
-from .v1_os import VultrOS
-from .v1_plans import VultrPlans
-from .v1_regions import VultrRegions
-from .v1_reservedip import VultrReservedIP
-from .v1_server import VultrServer
-from .v1_snapshot import VultrSnapshot
-from .v1_sshkey import VultrSSHKey
-from .v1_startupscript import VultrStartupScript
-
+from .v2_account import VultrAcc
+from .v2_applications import VultrApp
+from .v2_backup import VultrBackup
+from .v2_baremetal import VultrBareMetal
+from .v2_billing import VultrBilling
+from .v2_blockstorage import VultrBlockStorage
+from .v2_dns import VultrDNS
+from .v2_firewall import VultrFirewall
+from .v2_instances import VultrInstances
+from .v2_iso import VultrISO
+from .v2_kubernetes import VultrKubernetes
+from .v2_loadbalancers import VultrLoadBalancers
+from .v2_objectstorage import VultrObjectStorage
+from .v2_operatingsystems import VultrOperatingSystems
+from .v2_plans import VultrPlans
+from .v2_privatenetworks import VultrPrivateNetworks
+from .v2_regions import VultrRegions
+from .v2_reservedips import VultrReservedIPs
+from .v2_snapshots import VultrSnapshots
+from .v2_sshkeys import VultrSSHKeys
+from .v2_startupscripts import VultrStartupScripts
+from .v2_users import VultrUsers
 
 class Vultr(VultrBase):
-    '''Public Vultr interface'''
-
-    # pylint: disable=too-many-instance-attributes
-    # The interface is large, but built correctly
-    def __init__(self, api_key):
-        VultrBase.__init__(self, api_key)
-        self.account = VultrAccount(api_key)
-        self.app = VultrApp(api_key)
-        self.backup = VultrBackup(api_key)
-        self.dns = VultrDNS(api_key)
-        self.firewall = VultrFirewall(api_key)
-        self.iso = VultrISO(api_key)
-        # pylint: disable=invalid-name
-        # OS is the Vultr API namespace name
-        self.os = VultrOS(api_key)
-        self.plans = VultrPlans(api_key)
-        self.regions = VultrRegions(api_key)
-        self.reservedip = VultrReservedIP(api_key)
-        self.server = VultrServer(api_key)
-        self.snapshot = VultrSnapshot(api_key)
-        self.sshkey = VultrSSHKey(api_key)
-        self.startupscript = VultrStartupScript(api_key)
+        def __init__(self, api_key):
+                VultrBase.__init__(self, api_key)
+                self.account = VultrAcc(api_key)
+                self.applications = VultrApp(api_key)
+                self.backup = VultrBackup(api_key)
+                self.baremetal = VultrBareMetal(api_key)
+                self.billing = VultrBilling(api_key)
+                self.blockstorage = VultrBlockStorage(api_key)
+                self.dns = VultrDNS(api_key)
+                self.firewall = VultrFirewall(api_key)
+                self.instances = VultrInstances(api_key)
+                self.iso = VultrISO(api_key)
+                self.kubernetes = VultrKubernetes(api_key)
+                self.loadbalancers = VultrLoadBalancers(api_key)
+                self.objectstorage = VultrObjectStorage(api_key)
+                self.operatingsystems = VultrOperatingSystems(api_key)
+                self.plans = VultrPlans(api_key)
+                self.privatenetworks = VultrPrivateNetworks(api_key)
+                self.regions = VultrRegions(api_key)
+                self.reservedips = VultrReservedIPs(api_key)
+                self.snapshots = VultrSnapshots(api_key)
+                self.sshkeys = VultrSSHKeys(api_key)
+                self.startupscripts = VultrStartupScripts(api_key)
+                self.users = VultrUsers(api_key)
