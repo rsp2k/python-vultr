92bf59aebbff1ee9a9cecc1c7532ad190b327161
mgodiya <45644307+mgodiya@users.noreply.github.com>
2022-01-15 02:58:46 -0700

    Create v2_backup.py

---
+++ vultr/v2_backup.py
@@ -0,0 +1,16 @@
+'''Partial class to handle Vultr DNS API calls'''
+from .utils import VultrBase, update_params
+
+
+class VultrBackup(VultrBase):
+    '''Handles Vultr DNS API calls'''
+    def __init__(self, api_key):
+        VultrBase.__init__(self, api_key)
+
+    def list_backups(self, params=None):
+        params = params if params else dict()
+        return self.request('/v2/backups', params, 'GET')
+
+    def get_backup(self, backupid, params=None):
+        params = params if params else dict()
+        return self.request('/v2/backups/'+backupid, params, 'GET')
