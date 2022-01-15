705718cc909114a7d92dccb0576f6021e1202351
mgodiya <45644307+mgodiya@users.noreply.github.com>
2022-01-15 02:59:25 -0700

    Create v2_account.py

---
+++ vultr/v2_account.py
@@ -0,0 +1,12 @@
+'''Partial class to handle Vultr DNS API calls'''
+from .utils import VultrBase, update_params
+
+
+class VultrAcc(VultrBase):
+    '''Handles Vultr DNS API calls'''
+    def __init__(self, api_key):
+        VultrBase.__init__(self, api_key)
+
+    def account_info(self, params=None):
+        params = params if params else dict()
+        return self.request('/v2/account', params, 'GET')
